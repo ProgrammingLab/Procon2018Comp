1b1c0baac345a9b6cde007a56789c831f494fe88
うぃんじー <wing3196@gmail.com>
2018-04-11 14:32:49 +0900

    ちゃんと実装をcppに移した
    多重定義が起きていた

--- Procon2018/stdafx.cpp
+++ Procon2018/stdafx.cpp
@@ -1,2 +1,13 @@
 #include "stdafx.h"
 
+namespace Procon2018 {
+
+
+	s3d::Point Neighbour8(Direction8 dir) {
+		int dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+		int dy[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+		return s3d::Point(dx[dir], dy[dir]);
+	}
+
+
+}
--- Procon2018/stdafx.h
+++ Procon2018/stdafx.h
@@ -19,11 +19,7 @@ enum Direction8 {
 };
 
 
-s3d::Point Neighbour8(Direction8 dir) {
-	int dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
-	int dy[8] = {0, -1, -1, -1, 0, 1, 1, 1};
-	return s3d::Point(dx[dir], dy[dir]);
-}
+s3d::Point Neighbour8(Direction8 dir);
 
 
 }
