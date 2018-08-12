ccf7d8922ee5756765b9d686ddf405fcae596a6d
うぃんじー <wing3196@gmail.com>
2018-08-13 02:37:47 +0900

    8近傍と整数の対応をPythonのコードと合わせた

--- Procon2018/stdafx.cpp
+++ Procon2018/stdafx.cpp
@@ -4,8 +4,8 @@ namespace Procon2018 {
 
 
 	s3d::Point Neighbour8(Direction8 dir) {
-		int dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
-		int dy[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+		int dx[8] = {1, 0, -1, -1, -1, 0, 1, 1};
+		int dy[8] = {1, 1, 1, 0, -1, -1, -1, 0};
 		return s3d::Point(dx[dir], dy[dir]);
 	}
 
