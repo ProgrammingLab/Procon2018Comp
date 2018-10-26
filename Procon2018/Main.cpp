ecb7896d49572aab28859ca89c857e6e0ca09340
うぃんじー <wing3196@gmail.com>
2018-10-27 02:52:54 +0900

    AIの実験中１￥

--- Procon2018/Main.cpp
+++ Procon2018/Main.cpp
@@ -103,8 +103,8 @@ void HumanPlay() {
 
 void BattleToWinjAI() {
 	SP<AI> winjAI3((AI*)new WinjAI::WinjAI3());
-	SP<AI> winjAI2((AI*)new WinjAI::WinjAI2());
-	Playground grd(s3d::RectF(0, 0, s3d::Window::Size()), nullptr, winjAI3);
+	SP<AI> winjAI4((AI*)new WinjAI::WinjAI4());
+	Playground grd(s3d::RectF(0, 0, s3d::Window::Size()), winjAI3, winjAI4);
 	grd.setHiddenAI(true);
 	while (s3d::System::Update()) {
 		grd.update();
